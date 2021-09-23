4958a30e5b89388b5157d3b10cd15a867838ab81
JevexEndo <jevex.endo@gmail.com>
2021-09-23 00:29:12 -0500

    Updated QCardWidget

--- src/QtDesign/QtdWidgets.py
+++ src/QtDesign/QtdWidgets.py
@@ -1,7 +1,7 @@
 from typing import Optional, overload
 from PySide2.QtCore import Qt
 from PySide2.QtGui import QTextDocument
-from PySide2.QtWidgets import QGridLayout, QLabel, QPushButton, QTabBar, QTabWidget, QWidget
+from PySide2.QtWidgets import QGridLayout, QLabel, QLayout, QPushButton, QTabBar, QTabWidget, QWidget
 
 
 class QCardWidget(QWidget):
@@ -9,19 +9,18 @@ class QCardWidget(QWidget):
         super().__init__(parent)
 
         # Create QGridLayout as the layout manager for all Card widgets
-        self.cardLayout = QGridLayout()
+        self.__layout = QGridLayout()
+        self.__layout.setSizeConstraint(QLayout.SetMinimumSize)
 
         # Set QPushButton as the container for Card widgets
-        self.centralContainer = QPushButton()
-        self.centralContainer.setLayout(self.cardLayout)
+        self.__button = QPushButton()
+        self.__button.setLayout(self.__layout)
 
         # Create QGridLayout so QPushButton can be added to QWidget
-        self.centralLayout = QGridLayout()
-        self.centralLayout.addWidget(self.centralContainer)
-        self.setLayout(self.centralLayout)
-
-        # Make QPushButton fill QGridLayout
-        self.centralLayout.setContentsMargins(0, 0, 0, 0)
+        layout = QGridLayout()
+        layout.addWidget(self.__button)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
 
     @overload
     def addWidget(self, arg__1: QWidget, row: int, column: int, rowSpan: int, columnSpan: int, alignment: Optional[Qt.Alignment]) -> None: ...
@@ -31,31 +30,30 @@ class QCardWidget(QWidget):
     def addWidget(self, w: QWidget) -> None: ...
     
     def addWidget(self, *args, **kwargs) -> None:
-        widget      = args[0] if len(args) > 0 else kwargs.get("arg__1", kwargs["w"])
-        row         = args[1] if len(args) > 1 else kwargs.get("row", None)
-        column      = args[2] if len(args) > 2 else kwargs.get("column", None)
-        rowSpan     = args[3] if len(args) > 3 else kwargs.get("rowSpan", None)
-        columnSpan  = args[4] if len(args) > 4 else kwargs.get("columnSpan", None)
-        alignment   = args[5] if len(args) > 5 else kwargs.get("alignment", None)
-
-        widget.setParent(self.centralContainer)
+        widget: QWidget = args[0] if len(args) > 0 else kwargs.get("arg__1", kwargs["w"])
+        row: Optional[int] = args[1] if len(args) > 1 else kwargs.get("row", None)
+        column: Optional[int] = args[2] if len(args) > 2 else kwargs.get("column", None)
+        rowSpan: Optional[int] = args[3] if len(args) > 3 else kwargs.get("rowSpan", None)
+        columnSpan: Optional[int] = args[4] if len(args) > 4 else kwargs.get("columnSpan", None)
+        alignment: Optional[Qt.Alignment] = args[5] if len(args) > 5 else kwargs.get("alignment", None)
 
         if not None in [row, column, rowSpan, columnSpan, alignment]:
-            self.cardLayout.addWidget(widget, row, column, rowSpan, columnSpan, alignment)
+            self.__layout.addWidget(widget, row, column, rowSpan, columnSpan, alignment)
         elif not None in [row, column, rowSpan, columnSpan]:
-            self.cardLayout.addWidget(widget, row, column, rowSpan, columnSpan)
+            self.__layout.addWidget(widget, row, column, rowSpan, columnSpan)
         elif not None in [row, column, alignment]:
-            self.cardLayout.addWidget(widget, row, column, alignment)
+            self.__layout.addWidget(widget, row, column, alignment)
         elif not None in [row, column]:
-            self.cardLayout.addWidget(widget, row, column)
+            self.__layout.addWidget(widget, row, column)
         else:
-            self.cardLayout.addWidget(widget)
+            self.__layout.addWidget(widget)
 
-        self.centralContainer.setMinimumSize(self.cardLayout.sizeHint())
+    def layout(self) -> QLayout:
+        return self.__layout
 
     @property
     def clicked(self):
-        return self.centralContainer.clicked
+        return self.__button.clicked
 
 class QRichTabBar(QTabBar):
     def __init__(self, parent):
